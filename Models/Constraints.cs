6186c15254adbd4ad5c8e741afbcd36bd04d89c9
Aditya Roy Choudhary <arclive202@gmail.com>
2018-02-20 17:14:57 +0530

    Add files via upload

---
+++ Models/Constraints.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace WebApplication2.Models
+{
+    class Constraints
+    {
+        //FUNCTION TO VALIDATE SIGN IN
+        public static int validate(string username,string password)
+        {
+            int flag = 0;
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CGICon"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("select * from CustomerDetails where UserName = @username and [Password] = @password", con);
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+            SqlDataReader dr = cmd.ExecuteReader(); //You can use ExecuteScalar() to read the first column of first row.
+            if (dr.HasRows)
+                flag = 1;
+            
+            return flag;
+        }
+        //Function to generate patient ID. It gets the last existing record of patient from the database,
+        //and increments that value to generate the new patient id.
+        public static string GeneratePatientID()
+        {
+            //Connection created
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CGICon"].ConnectionString);
+            //Define a sql command to find out the maximum value of Customer ID from CustomerDetails table in the database.
+            //We will increment the value and then append the appropriate prefix to generate the new ID.
+            SqlCommand cmd = new SqlCommand("select CustomerID from CustomerDetails order by CustomerID desc", con);
+            //check if the connection is already open or closed. If it is closed, then open it.
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            string fullid = "";
+            SqlDataReader dr = cmd.ExecuteReader(); //You can use ExecuteScalar() to read the first column of first row.
+            if (dr.HasRows)
+            {
+                dr.Read();
+                string id = dr["CustomerID"].ToString();
+                int newid = int.Parse(id.Substring(1, id.Length - 1));
+                newid++;
+                if (newid < 10)
+                    fullid = "C00" + newid;
+                else if (newid >= 10 && newid < 100)
+                    fullid = "C0" + newid;
+                else 
+                    fullid = "C" + newid;
+                
+            }
+            else
+            {
+                fullid = "C001";
+            }
+            return fullid;
+        }
+        //phone number validation
+        public static bool IsValidPhone(string Phone)
+        {
+            var r = new Regex(@"^([0]|\+91)?[789]\d{9}$");
+            try
+            {
+                if (string.IsNullOrEmpty(Phone))
+                    return false;
+                return r.IsMatch(Phone);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        //email validation
+        public static bool emailIsValid(string email)
+        {
+            string expresion;
+            expresion = "\\w+([-+.']\\w+)*@\\w+([-.]\\w+)*\\.\\w+([-.]\\w+)*";
+            if (Regex.IsMatch(email, expresion))
+            {
+                if (Regex.Replace(email, expresion, string.Empty).Length == 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Models/Insert.cs
@@ -0,0 +1,88 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace WebApplication2.Models
+{
+    class Insert
+    {
+        public static int exists(string phone,string email)
+        {
+            int res = 0;
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CGICon"].ConnectionString);
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            SqlCommand cmd = new SqlCommand("select * from CustomerDetails where MobileNo = @Phone and EmailID = @email", con);
+            
+            cmd.Parameters.AddWithValue("@Phone", phone);
+            cmd.Parameters.AddWithValue("@Email", email);
+
+            res = cmd.ExecuteNonQuery();
+            return res;
+        }
+        public static int AddData(DateTime RegDate,string CustomerID,string CustomerName,string City,string MobileNo,string Address,string Email,string Pincode,string username,string password)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CGICon"].ConnectionString);
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            SqlCommand cmd = new SqlCommand("uspinsemp", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@regdate", RegDate);
+            cmd.Parameters.AddWithValue("@CustomerID", CustomerID);
+            cmd.Parameters.AddWithValue("@CustomerName", CustomerName);
+            cmd.Parameters.AddWithValue("@City", City);
+            cmd.Parameters.AddWithValue("@MobileNo", MobileNo);
+            cmd.Parameters.AddWithValue("@address", Address);
+            cmd.Parameters.AddWithValue("@email", Email);
+            cmd.Parameters.AddWithValue("@Pcode", Pincode);
+            cmd.Parameters.AddWithValue("@username", username);
+            cmd.Parameters.AddWithValue("@password", password);
+
+            int result = cmd.ExecuteNonQuery();
+            return result;
+        }
+        public static int ProductAddData(string ProductName, byte[] ProductImage, double Price,int Quantity)
+        {
+
+
+            int res = 0;
+
+            //double Price 
+            //int Quantity = 0;
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CGICon"].ConnectionString);
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            SqlCommand cmd = new SqlCommand("uspinsProduct1", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@ProductName", ProductName);
+            cmd.Parameters.AddWithValue("@Price", Price);
+            cmd.Parameters.AddWithValue("@Quantity", Quantity);
+            cmd.Parameters.AddWithValue("@ProductImage", ProductImage);
+
+            res = cmd.ExecuteNonQuery();
+                
+
+            return res;
+        }
+        public  DataTable DisplayAllProduct()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["CGICon"].ConnectionString);
+            SqlCommand cmd = new SqlCommand("Select ProductId,ProductName,ProductImage from Product", con);
+            if (con.State == ConnectionState.Closed)
+                con.Open();
+            SqlDataReader rdr = cmd.ExecuteReader();
+            DataTable dt = new DataTable();
+            if (rdr.HasRows)
+            {
+                dt.Load(rdr);
+            }
+            return dt;
+        }
+    }
+}
